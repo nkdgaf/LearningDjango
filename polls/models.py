9e736b48e04a308a74f4d25900d978c8d25205d6
Oware Gyekye <oware@Owares-MacBook-Air.local>
2026-05-09 11:44:54 +0000

    Initial Django push

---
+++ polls/models.py
@@ -0,0 +1,28 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone 
+
+
+class Question(models.Model):
+    questiontext = models.CharField(max_length=200)
+    pubdate = models.DateTimeField("date published")
+    def __str__(self):
+        return self.questiontext
+    
+
+    def waspublishedrecently(self):
+        return self.pubdate >= timezone.now() 
+datetime.timedelta(days=1)
+
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question,
+        on_delete=models.CASCADE)
+    choicetext = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.choicetext
+    
